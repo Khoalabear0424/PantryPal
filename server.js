a4851cb007403b927cd1a968badb42dd7846f1e9
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-25 17:22:45 -0700

    changed port

--- server.js
+++ server.js
@@ -206,6 +206,6 @@ app.get('*', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => {
+app.listen((process.env.PORT || 3000), () => {
     console.log('Listening on port 3000...')
 })
